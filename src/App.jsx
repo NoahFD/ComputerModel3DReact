72866473bc8bd6ccb554fd45493a4521af4ca058
NoahFD <weipingtong128@gmail.com>
2024-06-13 08:11:00 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import {Suspense, useRef,useState,useEffect} from 'react'
+import {Canvas, useFrame,useThree} from '@react-three/fiber'
+import './App.css'
+import {ComputerModel} from "./components/ComputerModel.jsx";
+import {OrbitControls} from "@react-three/drei";
+import * as THREE from 'three';
+import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing'
+
+
+function SpotAndPointLightComponent({ spotLightProps, pointLightProps }) {
+    const spotLightRef = useRef();
+    const pointLightRef = useRef();
+    const [helper, setHelper] = useState(null);
+
+    const { scene } = useThree();
+
+    useEffect(() => {
+        if (spotLightRef.current) {
+            const helper = new THREE.SpotLightHelper(spotLightRef.current);
+            // setHelper(helper);
+            // scene.add(helper);
+            return () => {
+                scene.remove(helper);
+            };
+        }
+    }, [scene]);
+
+    useFrame(() => {
+        if (spotLightRef.current) {
+            spotLightRef.current.target.position.set(0, 0, 0); // Make the spotlight target the center
+            if (helper) {
+                helper.update();
+            }
+        }
+    });
+
+    return (
+        <group>
+            <spotLight ref={spotLightRef} {...spotLightProps} />
+            {helper && <primitive object={helper} />}
+            <pointLight ref={pointLightRef} {...pointLightProps} />
+        </group>
+    );
+}
+
+
+function App() {
+
+    return (
+        <div style={{width: '100vw', height: '100vh'}}>
+            <Canvas camera={{position: [0, 5, 10], fov: 50}}shadows dpr={[1, 1.5]} >
+                <ambientLight intensity={0.5}/>
+                <directionalLight
+                    castShadow
+                    position={[10, 10, 10]}
+                    intensity={1.5}
+                    shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                    shadow-camera-far={50}
+                    shadow-camera-left={-10}
+                    shadow-camera-right={10}
+                    shadow-camera-top={10}
+                    shadow-camera-bottom={-10}
+                />
+                <pointLight position={[-10, -10, -10]} intensity={0.5}/>
+
+                {/* First Light Combination */}
+                <SpotAndPointLightComponent
+                    spotLightProps={{
+                        position: [-5.5779, 0, 13.835],
+                        angle: Math.PI / 4,
+                        penumbra: 0.5,
+                        intensity: 15,
+                        distance: 50,
+                        color: "#00FFFF",
+                        castShadow: true
+                    }}
+                    pointLightProps={{
+                        position: [-5.5779, 0, 13.835],
+                        intensity: 50,
+                        color: "#00FFFF"
+                    }}
+                />
+
+                {/* Second Light Combination */}
+                <SpotAndPointLightComponent
+                    spotLightProps={{
+                        position: [-7.7944, 8.4728, 8.0389],
+                        angle: Math.PI / 4,
+                        penumbra: 0.5,
+                        intensity: 100,
+                        distance: 30,
+                        color: "#fa6710",
+                        castShadow: true
+                    }}
+                    pointLightProps={{
+                        position: [-7.7944, 8.4728, 8.0389],
+                        intensity: 100,
+                        color: "#fa6710"
+                    }}
+                />
+
+                <Suspense fallback={null}>
+                    <ComputerModel/>
+                </Suspense>
+                <OrbitControls/>
+            </Canvas>
+        </div>
+
+    )
+}
+
+export default App
---
+++ src/components/ComputerTest.jsx
@@ -0,0 +1,199 @@
+import React, { useRef } from 'react';
+import { useGLTF, PerspectiveCamera, RenderTexture, Text } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+
+function Screen({ frame, children, ...props }) {
+    const { nodes, materials } = useGLTF('/old_computer.glb');
+    return (
+        <group {...props}>
+            <mesh castShadow receiveShadow geometry={nodes[frame].geometry} material={materials.Black} />
+            <mesh geometry={nodes[frame].geometry}>
+                <meshBasicMaterial toneMapped={false}>
+                    <RenderTexture width={512} height={512} attach="map" anisotropy={16}>
+                        {children}
+                    </RenderTexture>
+                </meshBasicMaterial>
+            </mesh>
+        </group>
+    );
+}
+
+function ScreenText({ x = 0, y = 1.2, ...props }) {
+    const textRef = useRef();
+    const rand = Math.random() * 10000;
+    useFrame((state) => {
+        if (textRef.current) {
+            textRef.current.position.x = x + Math.sin(rand + state.clock.elapsedTime / 4) * 8;
+        }
+    });
+    return (
+        <Screen {...props}>
+            <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0, 15]} />
+            <color attach="background" args={['black']} />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} />
+            <Text font="/Inter-Medium.woff" position={[x, y, 0]} ref={textRef} fontSize={4} letterSpacing={-0.1} color='white'>
+                Poimandres.
+            </Text>
+        </Screen>
+    );
+}
+
+export function ComputerModel(props) {
+    const { nodes, materials } = useGLTF('/old_computer.glb');
+    return (
+        <group {...props} dispose={null}>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.PCButton01.geometry}
+                material={materials.Blue}
+                position={[0.986, 0.873, 1.929]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.PCButton02.geometry}
+                material={materials.Red}
+                position={[1.49, 0.873, 1.949]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MonitorBackPart.geometry}
+                material={materials.Grey}
+                position={[0, 1.549, 0]}
+            />
+            <ScreenText frame="ComputerScreen" position={[0, 1.549, 0]} />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MonitorButton01.geometry}
+                material={materials.Blue}
+                position={[0.698, 1.892, 1.768]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MonitorButton02.geometry}
+                material={materials.Yellow}
+                position={[1.01, 1.892, 1.768]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.KeyboardBase.geometry}
+                material={materials.White}
+                position={[0, 0, 3.5]}
+            />
+            <group position={[0, 0, 3.5]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane021.geometry}
+                    material={materials.White}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane021_1.geometry}
+                    material={materials.Yellow}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane021_2.geometry}
+                    material={materials.DarkGrey}
+                />
+            </group>
+            <group position={[3.267, 0, 3.313]} rotation={[0, 0.241, 0]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane017.geometry}
+                    material={materials.Grey}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane017_1.geometry}
+                    material={materials.Grey}
+                />
+            </group>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MouseSide.geometry}
+                material={materials.Grey}
+                position={[3.267, 0, 3.313]}
+                rotation={[0, 0.241, 0]}
+            />
+            <group position={[3.267, 0, 3.313]} rotation={[0, 0.241, 0]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane022.geometry}
+                    material={materials.Grey}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane022_1.geometry}
+                    material={materials.Grey}
+                />
+            </group>
+            <group position={[3.267, 0, 3.313]} rotation={[0, 0.241, 0]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane011.geometry}
+                    material={materials.White}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane011_1.geometry}
+                    material={materials.White}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Plane011_2.geometry}
+                    material={materials.White}
+                />
+            </group>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MouseCable.geometry}
+                material={nodes.MouseCable.material}
+                position={[1.272, 0.712, -2.003]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.PCPoint.geometry}
+                material={nodes.PCPoint.material}
+                position={[1.272, 0.712, -2.003]}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.MonitorFrontPart.geometry}
+                material={materials.White}
+                position={[0, 1.549, 0]}
+            />
+            <mesh castShadow receiveShadow geometry={nodes.Plane023.geometry} material={materials.Grey} />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Plane023_1.geometry}
+                material={materials.Grey}
+            />
+            <mesh castShadow receiveShadow geometry={nodes.Plane.geometry} material={materials.White} />
+            <mesh castShadow receiveShadow geometry={nodes.Plane_1.geometry} material={materials.White} />
+        </group>
+    );
+}
+
+useGLTF.preload('/old_computer.glb');
